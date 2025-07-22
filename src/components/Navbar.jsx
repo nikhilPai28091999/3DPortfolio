209082509042db3c36d78cf2010ee9ae0e1e1cd7
Nikhil <2809nikhilpai@gmail.com>
2025-07-22 20:05:25 +0400

    First commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { styles } from "../styles";
+import { Link } from "react-router-dom";
+import { logo } from "../assets";
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+  console.log("debug");
+  const [active, setActive] = useState(" ");
+  return (
+    <nav
+      className={`
+    ${styles.paddingX} w-full flex
+    items-center py-5 fixed top-0 z-20 bg-primary
+    `}
+    >
+      <div className="w-full flex justify-between items-start max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img
+            src={logo}
+            alt="logo"
+            className="w-9
+        h-9 object-contain"
+          />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Nikhil Pai
+          </p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover: text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/canvas/Ball.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Ball = (props) => {
+  return <div>{props.name}</div>;
+};
+
+export default Ball;
